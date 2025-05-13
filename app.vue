d0e869e62ff0791663c0a44ba4fd7abb010df177
Lukas Olivier <olivier.lukas2003@gmail.com>
2025-05-13 11:56:57 +0200

    chore: installed vercel analytics package

--- app.vue
+++ app.vue
@@ -1,7 +1,12 @@
 <template>
   <div>
     <NuxtLayout>
+      <Analytics />
       <NuxtPage />
     </NuxtLayout>
   </div>
 </template>
+
+<script setup>
+import { Analytics } from "@vercel/analytics/nuxt";
+</script>
